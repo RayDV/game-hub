6061768ded43a8b333987b8253c330fa0070b2be
Ray Devera <rayd808@yahoo.com>
2025-01-02 17:11:29 -0800

    Adding Emojis

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,11 +1,12 @@
-import { Game } from '../hooks/useGames'
-import { Card, CardBody, Heading, HStack, Image } from '@chakra-ui/react'
-import PlatformIconList from './PlatformIconList'
-import CriticScore from './CriticScore'
-import getCroppedImageUrl from '../services/image-urls'
+import { Game } from "../hooks/useGames";
+import { Card, CardBody, Heading, HStack, Image } from "@chakra-ui/react";
+import PlatformIconList from "./PlatformIconList";
+import CriticScore from "./CriticScore";
+import getCroppedImageUrl from "../services/image-urls";
+import Emoji from "./Emoji";
 
 interface Props {
-  game: Game
+  game: Game;
 }
 
 const GameCard = ({ game }: Props) => {
@@ -13,14 +14,19 @@ const GameCard = ({ game }: Props) => {
     <Card>
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
-        <HStack justifyContent='space-between' marginBottom={3}>
-          <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)} />
-          <CriticScore score={game.metacritic}/>
+        <HStack justifyContent="space-between" marginBottom={3}>
+          <PlatformIconList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
+          <CriticScore score={game.metacritic} />
         </HStack>
-        <Heading fontSize='2xl'>{game.name}</Heading>
+        <Heading fontSize="2xl">
+          {game.name}
+          <Emoji rating={game.rating_top} />
+        </Heading>
       </CardBody>
     </Card>
-  )
-}
+  );
+};
 
-export default GameCard
+export default GameCard;
